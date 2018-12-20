4a0a3c0397a34bbb0513365d01210dbb20ba502a
franspaco <franspaco@gmail.com>
2018-12-20 18:20:45 +0100

    frontend

--- frontend/js/map.js
+++ frontend/js/map.js
@@ -8,9 +8,7 @@ function initMap() {
         center: myLatLng
     });
 
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: 'Hello World!'
+    $.getJSON("/data/teams.json", (data)=> {
+        
     });
 }
